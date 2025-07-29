ef4ee3d054e6e2bd85f1866f557be7d5addc47de
GuusF <70903780+GuusF@users.noreply.github.com>
2025-07-29 02:51:43 -0700

    Add minimap extension (__init__.py)

---
+++ custom_nodes/minimap_extension/__init__.py
@@ -0,0 +1,10 @@
+"""
+Minimap extension for ComfyUI.
+
+Placing this directory inside the ``custom_nodes`` folder allows ComfyUI
+to automatically serve the JavaScript asset contained herein.  There are no
+custom nodes defined – all of the interactive behaviour lives in
+``minimap.js``.  See that file for implementation details.
+"""
+
+WEB_DIRECTORY = "."
